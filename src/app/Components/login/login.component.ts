6b8fb66ac85ec93e13cb3e50e2897176db59d30f
victor Gilbert <victorgilbert63@gmail.com>
2023-07-05 09:54:21 +0100

    Login Form Validation

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,12 +10,29 @@ export class LoginComponent {
   type: string = "password"
   isText: boolean = false;
   eyeIcon: string = " fa-eye-slash";
-
- 
+  loginForm!: FormGroup;
+  
+constructor(private fb: FormBuilder){
+  
+}
+ngOnInit(): void{
+  this.loginForm = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+}
   hideShowPass(){
     this.isText = !this.isText;
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
     this.isText ? this.type = "text" : this.type = "password";
   }
+  onSubmit(){
+    if(this.loginForm.valid){
+      //send oblject to database
+    }else{
+      //throw error
+    }
+  }
   
 }
+ 
